263cba4ca971845b6b0e088fff52dec4e5b658e4
aelew <118043566+aelew@users.noreply.github.com>
2024-01-24 00:04:42 -0800

    fix cloudflare setup

--- remix.config.js
+++ remix.config.js
@@ -1,12 +1,12 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   ignoredRouteFiles: ['**/.*'],
+  tailwind: true,
+  postcss: true,
   server: './server.ts',
+  serverBuildPath: 'functions/[[path]].js',
   serverConditions: ['workerd', 'worker', 'browser'],
-  serverDependenciesToBundle: [
-    // bundle everything except the virtual module for the static content manifest provided by wrangler
-    /^(?!.*\b__STATIC_CONTENT_MANIFEST\b).*$/
-  ],
+  serverDependenciesToBundle: 'all',
   serverMainFields: ['browser', 'module', 'main'],
   serverMinify: true,
   serverModuleFormat: 'esm',
@@ -14,5 +14,4 @@ export default {
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
-  // serverBuildPath: "build/index.js",
 };

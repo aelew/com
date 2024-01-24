78bc14b124d4a6454501333dad4e98c10863c6d8
Andre Lew <me@aelew.com>
2024-01-23 23:45:53 -0800

    use cloudflare workers (#2)

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,4 @@
-import type { MetaFunction } from '@remix-run/node';
+import type { MetaFunction } from '@remix-run/cloudflare';
 import { motion } from 'framer-motion';
 
 import { ProjectList } from '~/components/home/project-list';

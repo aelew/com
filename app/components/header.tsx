d5b3fb643830b12b58733759b117b30f842683e7
aelew <118043566+aelew@users.noreply.github.com>
2024-01-30 00:59:06 -0800

    fix header styling

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -6,31 +6,26 @@ import { ThemeSwitcher } from './theme-switcher';
 export function Header() {
   return (
     <motion.header
-      className="sticky top-0 z-50 backdrop-blur dark:bg-background/25"
+      className="sticky top-0 z-50 border-b bg-background backdrop-blur supports-[backdrop-filter]:bg-background/25 dark:border-border/50"
+      transition={{ type: 'tween' }}
       initial={{ y: -5, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
-      transition={{ type: 'tween' }}
     >
-      <div className="container">
-        <div className="flex items-center justify-between border-b py-4">
-          <Link
-            className="flex gap-3 transition-opacity hover:opacity-80"
-            to="/"
-          >
-            <img
-              className="rounded-md shadow"
-              src="/images/penguin.jpg"
-              alt="Penguin"
-              height={32}
-              width={32}
-            />
-            <span className="text-gradient text-xl font-semibold tracking-tight">
-              aelew
-            </span>
-          </Link>
-          <div>
-            <ThemeSwitcher />
-          </div>
+      <div className="container flex items-center justify-between py-4">
+        <Link className="flex gap-3 transition-opacity hover:opacity-80" to="/">
+          <img
+            className="rounded-md shadow"
+            src="/images/penguin.jpg"
+            alt="Penguin"
+            height={32}
+            width={32}
+          />
+          <span className="text-gradient text-xl font-semibold tracking-tight">
+            aelew
+          </span>
+        </Link>
+        <div>
+          <ThemeSwitcher />
         </div>
       </div>
     </motion.header>

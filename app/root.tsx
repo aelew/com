e779125c23e191d27c53b8ceb08ea7ea0779f6bd
aelew <118043566+aelew@users.noreply.github.com>
2024-02-01 09:26:49 -0800

    Revert "update analytics subdomain due to adblockers"
    This reverts commit f911ccceb92917037da1ae664e6dce057211bec5.

--- app/root.tsx
+++ app/root.tsx
@@ -46,7 +46,7 @@ export function App() {
         <PreventFlashOnWrongTheme ssrTheme={Boolean(data.theme)} />
         <Links />
         <script
-          src="https://stats.aelew.dev/js/script.outbound-links.js"
+          src="https://analytics.aelew.dev/js/script.outbound-links.js"
           data-domain="aelew.com"
           defer
         />

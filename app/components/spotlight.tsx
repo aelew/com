710596109cb900a55f5fc08a8857495fb2609e70
aelew <me@aelew.com>
2024-07-16 16:12:07 -0700

    format

--- app/components/spotlight.tsx
+++ app/components/spotlight.tsx
@@ -9,7 +9,7 @@ export function Spotlight({ ellipseClassName, className }: SpotlightProps) {
   return (
     <svg
       className={cn(
-        'animate-spotlight pointer-events-none absolute z-[1] h-[169%] w-[138%] opacity-0 lg:w-[84%]',
+        'pointer-events-none absolute z-[1] h-[169%] w-[138%] animate-spotlight opacity-0 lg:w-[84%]',
         className
       )}
       xmlns="http://www.w3.org/2000/svg"

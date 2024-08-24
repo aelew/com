c8121c1c9e42dfbae295210a4fda8728496d78c1
aelew <me@aelew.com>
2024-08-24 16:46:01 -0700

    add `prettier-plugin-tailwindcss`

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,6 +1,10 @@
 /** @type {import('prettier').Config & import("@ianvs/prettier-plugin-sort-imports").PluginConfig} */
 const config = {
-  plugins: ['prettier-plugin-astro', '@ianvs/prettier-plugin-sort-imports'],
+  plugins: [
+    'prettier-plugin-astro',
+    'prettier-plugin-tailwindcss',
+    '@ianvs/prettier-plugin-sort-imports'
+  ],
   importOrder: ['<THIRD_PARTY_MODULES>', '', '^[../]', '^[./]'],
   importOrderParserPlugins: ['typescript', 'decorators-legacy'],
   trailingComma: 'none',

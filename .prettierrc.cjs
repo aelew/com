9c5ea5faff7bfc1d084e0043651f125142dfa4e7
aelew <me@aelew.com>
2024-08-24 16:42:29 -0700

    add prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config & import("@ianvs/prettier-plugin-sort-imports").PluginConfig} */
+const config = {
+  plugins: ['prettier-plugin-astro', '@ianvs/prettier-plugin-sort-imports'],
+  importOrder: ['<THIRD_PARTY_MODULES>', '', '^[../]', '^[./]'],
+  importOrderParserPlugins: ['typescript', 'decorators-legacy'],
+  trailingComma: 'none',
+  singleQuote: true
+};
+
+module.exports = config;

90bee3f904ed3cd20923526c5ed0af7db691c522
aelew <me@aelew.com>
2024-08-24 17:14:55 -0700

    fix prettier config

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,14 +1,22 @@
 /** @type {import('prettier').Config & import("@ianvs/prettier-plugin-sort-imports").PluginConfig} */
 const config = {
+  importOrder: ['<THIRD_PARTY_MODULES>', '', '^[../]', '^[./]'],
+  importOrderParserPlugins: ['typescript', 'decorators-legacy'],
+  trailingComma: 'none',
+  singleQuote: true,
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro'
+      }
+    }
+  ],
   plugins: [
     'prettier-plugin-astro',
     'prettier-plugin-tailwindcss',
     '@ianvs/prettier-plugin-sort-imports'
-  ],
-  importOrder: ['<THIRD_PARTY_MODULES>', '', '^[../]', '^[./]'],
-  importOrderParserPlugins: ['typescript', 'decorators-legacy'],
-  trailingComma: 'none',
-  singleQuote: true
+  ]
 };
 
 module.exports = config;

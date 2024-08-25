05b9d6281377887b5e8ab4a8224f432339e41996
aelew <me@aelew.com>
2024-08-24 22:35:42 -0700

    progress

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,21 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: ['Geist Sans', ...defaultTheme.fontFamily.sans]
+      },
+      container: {
+        center: true,
+        padding: '1rem',
+        screens: {
+          '2xl': '768px'
+        }
+      }
+    }
   },
   plugins: []
 };

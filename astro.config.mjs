37dcf3330657d63b964bd83bbe0934d2a85f98d3
aelew <me@aelew.com>
2024-08-24 17:14:47 -0700

    add `site` to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: import.meta.env.DEV ? 'http://localhost:4321' : 'https://aelew.com',
   integrations: [tailwind()]
 });

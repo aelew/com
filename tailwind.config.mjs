5fe144aaf94ffde805abbec212b0ab804bd9a18b
aelew <me@aelew.com>
2024-11-16 17:34:55 -0800

    fix card background

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -14,6 +14,12 @@ export default {
         screens: {
           '2xl': '768px'
         }
+      },
+      colors: {
+        neutral: {
+          ...defaultTheme.colors.neutral,
+          850: 'rgb(31 31 31 / var(--tw-bg-opacity, 1))'
+        }
       }
     }
   },

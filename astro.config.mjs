ea274362e285b0341d55492f9aa6100ee74bc4f1
aelew <me@aelew.com>
2024-12-11 18:14:36 -0800

    upgrade to astro v5

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  integrations: [tailwind()],
   site:
     process.env.NODE_ENV === 'production'
       ? 'https://aelew.com'
       : 'http://localhost:4321',
-  integrations: [tailwind()],
   image: {
     remotePatterns: [
       {
